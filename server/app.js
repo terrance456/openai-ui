530a644ba873a65ff4b8bef31fd242c76007c41e
terrance456 <terranceprasath97@gmail.com>
2023-03-04 18:50:32 +0800

    update dotenv

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const middleware = require("./middleware/index");
+const env = require("dotenv").config();
 
 const app = express();
 const port = 3001;
--- server/auth/firebase-config.js
+++ server/auth/firebase-config.js
@@ -1,17 +1,17 @@
 const admin = require("firebase-admin");
-const env = require("dotenv").config()?.parsed;
+const env = require("dotenv").config();
 
 const serviceAccount = {
   type: "service_account",
-  project_id: env.FIREBASE_ADMIN_PROJECT_ID,
-  private_key_id: env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
-  private_key: env.FIREBASE_ADMIN_PRIVATE_KEY,
-  client_email: env.FIREBASE_ADMIN_CLIENT_EMAIL,
-  client_id: env.FIREBASE_ADMIN_CLIENT_ID,
-  auth_uri: env.FIREBASE_ADMIN_AUTH_URI,
-  token_uri: env.FIREBASE_ADMIN_TOKEN_URI,
-  auth_provider_x509_cert_url: env.FIREBASE_ADMIN_auth_provider_x509_cert_url,
-  client_x509_cert_url: env.FIREBASE_ADMIN_client_x509_cert_url,
+  project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_ADMIN_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_ADMIN_AUTH_URI,
+  token_uri: process.env.FIREBASE_ADMIN_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_ADMIN_auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.FIREBASE_ADMIN_client_x509_cert_url,
 };
 
 if (admin.apps.length === 0) {
