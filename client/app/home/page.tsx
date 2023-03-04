4f4bfa47e6ce3a913792ee4723a293cec179dab0
terrance456 <terranceprasath97@gmail.com>
2023-03-04 14:58:29 +0800

    reorganize directory

---
+++ client/app/home/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LoginButton from "./LoginButton/LoginButton";
+
+export default async function About() {
+  return (
+    <div>
+      <LoginButton />
+    </div>
+  );
+}
