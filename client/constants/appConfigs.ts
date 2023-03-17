c22370a5295d7b619010812c2834889d5f9d2106
terrance456 <terranceprasath97@gmail.com>
2023-03-18 02:39:16 +0800

    update url

--- client/constants/appConfigs.ts
+++ client/constants/appConfigs.ts
@@ -1,3 +1,3 @@
 export const CommonConfigs = {
-  baseUrl: process.env.NODE_ENV === "production" ? "https://openaiuiapi.onrender.com/openai" : "http://localhost:3022",
+  baseUrl: process.env.NODE_ENV === "production" ? "https://openaiuiapi.onrender.com" : "http://localhost:3022",
 };
