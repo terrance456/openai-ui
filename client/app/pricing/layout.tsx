37845191e2b1e629f35e79db954093aa4f0e9483
terrance456 <terranceprasath97@gmail.com>
2023-11-16 23:52:24 +0800

    refactor: change layout method

--- client/app/pricing/layout.tsx
+++ client/app/pricing/layout.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { ProtectedRoute } from "@/src/components/ProtectedRoute";
-import PricingPage from "./page";
 import { Metadata } from "next";
+import { CommonLayoutProps } from "@/src/types/layout";
 
 export const metadata: Metadata = {
   title: "Pricing | Nexusphere AI",
   description: "Pricing in Nexusphere AI",
 };
 
-export default function PricingLayout() {
-  return (
-    <ProtectedRoute>
-      <PricingPage />
-    </ProtectedRoute>
-  );
+export default function PricingLayout({ children }: CommonLayoutProps) {
+  return <ProtectedRoute>{children}</ProtectedRoute>;
 }
