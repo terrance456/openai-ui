210efea468fb97d1197b953846583a5f6d56120e
terrance456 <terranceprasath97@gmail.com>
2023-11-17 18:55:12 +0800

    reafctor: remove get-invoice endpoint

--- server/routes/stripe/utils.ts
+++ server/routes/stripe/utils.ts
@@ -1,5 +1,16 @@
 import Stripe from "stripe";
 
+export interface FormattedPaymentHistory {
+  id: string;
+  invoice: string | null;
+  invoiceUrl: string | null;
+  purchased_date: number;
+  amount: number;
+  currency: string;
+  status: string;
+  productId: string | null;
+}
+
 export function retriveProductCredits(list: Array<Stripe.Product>, priceId: string) {
   const validItem: Stripe.Product = list.find((v) => v.default_price === priceId) as Stripe.Product;
 
@@ -18,14 +29,26 @@ export function retriveProductCredits(list: Array<Stripe.Product>, priceId: stri
   }
 }
 
-export function formatPaymentHistory(list: Stripe.PaymentIntent[], productList: Stripe.Product[]) {
+export function formatPaymentHistory(list: Stripe.PaymentIntent[], productList: Stripe.Product[]): FormattedPaymentHistory[] {
   return list.map((value: Stripe.PaymentIntent) => ({
     id: value.id,
-    invoice: value.invoice,
+    invoice: value.invoice as string,
     purchased_date: value.created,
     amount: value.amount,
     currency: value.currency,
     status: value.status,
     productId: productList.find((product: Stripe.Product) => product.default_price === value.metadata.productPrice)?.metadata?.clientId || null,
+    invoiceUrl: null,
   }));
+}
+
+export function formatPaymentHistoryWithInvoices(paymentHistory: FormattedPaymentHistory[], invoiceList: Stripe.Invoice[]) {
+  return paymentHistory.map((value: FormattedPaymentHistory) => {
+    const invoice: Stripe.Invoice = invoiceList.find((invoice: Stripe.Invoice) => invoice.id === value.invoice) as Stripe.Invoice;
+
+    if (invoice) {
+      return { ...value, invoiceUrl: invoice.hosted_invoice_url };
+    }
+    return value;
+  });
 }
